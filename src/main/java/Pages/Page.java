845eaa47660f61b99c126397f0e8ed098b02edc1
MohaimanulIslam <mohaimanul.cse@gmail.com>
2021-12-19 03:45:14 +0600

    Cucumber Selenium with page object model

---
+++ src/main/java/Pages/Page.java
@@ -0,0 +1,22 @@
+package Pages;
+
+import Core.ConfigUtil;
+import Core.WebConnector;
+import org.openqa.selenium.WebDriver;
+
+import java.util.Properties;
+
+public class Page {
+
+    public static Properties myProp = ConfigUtil.getConfig("config");
+    public WebDriver driver = WebConnector.driver;
+
+    LoginPage loginPage = new LoginPage(this);
+    protected LoginPage getLoginPage() { return loginPage; }
+
+    RegisterPage registerPage = new RegisterPage(this);
+    protected RegisterPage getRegisterPage(){return registerPage;}
+
+
+
+}
